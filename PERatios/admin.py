bb4e2ac83da67bfa0d3583dc5f72ef8f63041bdc
maxipytech <maxipytech@gmail.com>
2026-02-22 13:15:18 +0000

    models altered

--- PERatios/admin.py
+++ PERatios/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(PE_Data)
+# admin.site.register(Company_Data)
--- PERatios/models.py
+++ PERatios/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class PE_Data(models.Model):
     company_name = models.CharField(max_length=50, default='', blank=True, null=True)
-    current_share_price = models.CharField(max_length=50, default='', blank=True, null=True)
-    earnings_per_share = models.DecimalField(max_digits=5, decimal_places=2)
-    highest_earnings_per_share = models.DecimalField(max_digits=5, decimal_places=2)
+    current_share_price = models.DecimalField(max_digits=5, decimal_places=2, default='', blank=True, null=True)
+    earnings_per_share = models.DecimalField(max_digits=5, decimal_places=2, default='', blank=True, null=True)
+    highest_earnings_per_share = models.DecimalField(max_digits=5, decimal_places=2, default='', blank=True, null=True)
   
